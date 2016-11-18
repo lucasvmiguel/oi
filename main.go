92f38dac196362b209fea88c727e0de0045eb2c4
lucas <lucasvmiguel@live.com>
2016-11-18 00:20:02 -0200

    starting es work

--- main.go
+++ main.go
@@ -1,84 +1,167 @@
 package main
 
 import (
-	"github.com/jinzhu/gorm"
-	elastic "gopkg.in/olivere/elastic.v3"
+	"bytes"
+	"context"
+	"log"
+
+	elastic "gopkg.in/olivere/elastic.v5"
 )
+
+type Search struct {
+	ID               int
+	Question         string
+	Answer           string
+	QuestionSubjects []string
+	AnswerSubjects   []string
+	Suggestions      []string
+	Identity         string
+}
 
 func main() {
 	// conecta com o repositório das mensagens
-	db, err := gorm.Open("postgres", name)
-	if err != nil {
-		panic("postgres panic")
-	}
+	// db, err := gorm.Open("postgres", name)
+	// if err != nil {
+	// 	panic("postgres panic")
+	// }
 
-	defer db.Close()
+	// defer db.Close()
 
-	db.AutoMigrate(&Chat{})
-	db.AutoMigrate(&Message{})
+	// db.AutoMigrate(&Chat{})
+	// db.AutoMigrate(&Message{})
 
 	//conecta com a base das mensagens
-	client, err := elastic.NewClient()
+	client, err := elastic.NewClient(elastic.SetURL("http://localhost:9200"))
 	if err != nil {
 		panic("elastic panic")
 	}
 
-	_, err = client.CreateIndex("oi").Do()
-	if err != nil {
-		panic(err)
-	}
+	var lw bytes.Buffer
+	lout := log.New(&lw, "LOGGER ", log.LstdFlags)
 
-	client.conn.PutMapping().Index(indexName).Type(indexType).BodyJson(body).Do()
+	var tw bytes.Buffer
+	tout := log.New(&tw, "TRACER ", log.LstdFlags)
+
+	elastic.SetInfoLog(lout)
+	elastic.SetTraceLog(tout)
+
+	client.CreateIndex("oi").Do(context.TODO())
+
+	client.PutMapping().Index("oi").Type("message").BodyJson(
+		map[string]interface{}{
+			"properties": map[string]interface{}{
+				"Question": map[string]interface{}{
+					"type":     "string",
+					"analyzer": "brazilian",
+				},
+				"QuestionSubjects": map[string]interface{}{
+					"type":     "string",
+					"analyzer": "brazilian",
+				},
+				"Answer": map[string]interface{}{
+					"type":  "string",
+					"index": "not_analyzed",
+				},
+				"AnswerSubjects": map[string]interface{}{
+					"type":  "string",
+					"index": "not_analyzed",
+				},
+				"Suggestions": map[string]interface{}{
+					"type":  "string",
+					"index": "not_analyzed",
+				},
+				"Identity": map[string]interface{}{
+					"type":  "string",
+					"index": "not_analyzed",
+				},
+			},
+		}).Do(context.TODO())
+
+	// client.Index().
+	// 	Index("oi").
+	// 	Type("message").
+	// 	BodyJson(Search{
+	// 		Question:         "oi",
+	// 		Answer:           "oi, tudo bem?",
+	// 		QuestionSubjects: []string{"intro"},
+	// 		AnswerSubjects:   []string{"intro"},
+	// 		Suggestions:      []string{"sim", "não"},
+	// 		Identity:      	  "oi_simples",
+	// 	}).
+	// 	Do(context.TODO())
+
+	// client.Index().
+	// 	Index("oi").
+	// 	Type("message").
+	// 	BodyJson(Search{
+	// 		Question:         "olá",
+	// 		Answer:           "oi, tudo bem?",
+	// 		QuestionSubjects: []string{"intro"},
+	// 		AnswerSubjects:   []string{"intro"},
+	// 		Suggestions:      []string{"sim", "não"},
+	// 		Identity:      	  "ola_simples",
+	// 	}).
+	// 	Do(context.TODO())
 
 	//geração de uma pergunta qualquer
-	question := Question{
-		Message: "oi",
-		ChatID:  1,
-	}
+	// question := Question{
+	// 	Message: "oi",
+	// 	ChatID:  1,
+	// }
 
-	//verifica se ja tem algum chat já criado com aquele ID
-	chat := Chat{
-		ID: ChatID,
-	}
+	// //verifica se ja tem algum chat já criado com aquele ID
+	// chat := Chat{
+	// 	ID: ChatID,
+	// }
 
-	db.FirstOrCreate(&chat)
+	// db.FirstOrCreate(&chat)
 
-	search := Search{
-		Question: question.Message,
-	}
+	// search := Search{
+	// 	Question: question.Message,
+	// }
 
-	//se ja tiver chat ele vai pegar a ultima mensagem para saber do assunto tratado(contexto)
-	if len(chat.Messages) > 0 {
-		search.QuestionSubject = chat.Messages[len(chat.Messages)-1].Subject
-	}
+	// //se ja tiver chat ele vai pegar a ultima mensagem para saber do assunto tratado(contexto)
+	// if len(chat.Messages) > 0 {
+	// 	search.QuestionSubject = chat.Messages[len(chat.Messages)-1].Subject
+	// }
 
-	//pesquisa por uma mensagem correspondente com o assunto e a mensagem
-	searchResult, _ := client.Search().
-		Index("oi").
-		Query(elastic.NewTermQuery("question", question.Message)).
-		Query(elastic.NewTermQuery("questionSubject", question.QuestionSubject)).
-		From(0).Size(1).
-		Do()
+	// //pesquisa por uma mensagem correspondente com o assunto e a mensagem
 
-	//transforma em uma mensagem apartir da resposta do elasticsearch
-	answerMessage := Message{
-		Message: answerSearch.Message,
-		Subject: answerSearch.AnswerSubject,
-		ChatID:  chat.ID,
-	}
+	searchResult, err := client.Search().
+		Query(
+			elastic.NewBoolQuery().Should(
+				elastic.NewMatchQuery("Question", "oi").Boost(100),
+				elastic.NewMatchQuery("QuestionSubjects", "intro").Boost(100),
+			)).
+		From(0).
+		Size(1).
+		Do(context.TODO())
+	// searchResult, _ := client.Search().
+	// 	Index("oi").
+	// 	Query(elastic.NewTermQuery("question", question.Message)).
+	// 	Query(elastic.NewTermQuery("questionSubject", question.QuestionSubject)).
+	// 	From(0).Size(1).
+	// 	Do()
 
-	//inseri essa mensagem no chat
-	db.Create(&answerMessage)
+	// //transforma em uma mensagem apartir da resposta do elasticsearch
+	// answerMessage := Message{
+	// 	Message: answerSearch.Message,
+	// 	Subject: answerSearch.AnswerSubject,
+	// 	ChatID:  chat.ID,
+	// }
 
-	//pega todo o chat(essa parte não é realmente necessária,
-	//pode dar um append da obj mensagem em cima com o obj chat)
-	db.First(&chat)
+	// //inseri essa mensagem no chat
+	// db.Create(&answerMessage)
 
-	//tranforma toda resposta anterior para uma resposta do usuário
-	answerUser := Answer{
-		Message:      answerMessage.Message,
-		Suggestions:  searchResult.Suggestions,
-		Subject:      answerMessage.AnswerSubject,
-		Conversation: chat.Messages,
-	}
+	// //pega todo o chat(essa parte não é realmente necessária,
+	// //pode dar um append da obj mensagem em cima com o obj chat)
+	// db.First(&chat)
+
+	// //tranforma toda resposta anterior para uma resposta do usuário
+	// answerUser := Answer{
+	// 	Message:      answerMessage.Message,
+	// 	Suggestions:  searchResult.Suggestions,
+	// 	Subject:      answerMessage.AnswerSubject,
+	// 	Conversation: chat.Messages,
+	// }
 }
--- robot.go
+++ robot.go
@@ -1,76 +1,4 @@
-package oi
-
-type Search struct {
-	ID               int
-	Question         string
-	Answer           string
-	QuestionSubjects []string
-	AnswerSubjects   []string
-	Suggestions      []string
-}
-
-// CONFIG
-//
-// {
-//   "settings": {
-//     "analysis": {
-//       "filter": {
-//         "brazilian_stop": {
-//           "type":       "stop",
-//           "stopwords":  "_brazilian_"
-//         },
-//         "brazilian_keywords": {
-//           "type":       "keyword_marker",
-//           "keywords":   []
-//         },
-//         "brazilian_stemmer": {
-//           "type":       "stemmer",
-//           "language":   "brazilian"
-//         }
-//       },
-//       "analyzer": {
-//         "brazilian": {
-//           "tokenizer":  "standard",
-//           "filter": [
-//             "lowercase",
-//             "brazilian_stop",
-//             "brazilian_keywords",
-//             "brazilian_stemmer"
-//           ]
-//         }
-//       }
-//     }
-//   }
-// }
-
-// {
-//   "mappings": {
-//     "message" : {
-//       "properties" : {
-//         "question" : {
-//             "type" :    "string",
-//             "analyzer": "brazilian"
-//         },
-//         "questionSubjects": {
-//             "type" :    "string",
-//             "analyzer": "brazilian"
-//         },
-//         "answer" : {
-//             "type":     "string",
-//             "index":    "not_analyzed"
-//         },
-//         "answerSubjects": {
-//             "type" :    "string",
-//             "index":    "not_analyzed"
-//         },
-//         "suggestions" : {
-//           "type" :   "string",
-//           "index":    "not_analyzed"
-//         }
-//       }
-//     }
-//   }
-// }
+package robot
 
 // {"question" : "oi",
 // "questionSubjects": ["intro"],
@@ -79,23 +7,18 @@ type Search struct {
 // "suggestions" : ["sim", "não"]}
 
 // {"question" : "fala ai",
-// "questionSubjects": ["intro"],
+// "  ": ["intro"],
 // "answer" : "tranquilo, tudo bem?",
 // "answerSubjects": ["intro"],
 // "suggestions" : ["sim", "não"]}
 
 // {
-//   "query": {
-//       "should" : [
-//         { "match" : {
-//             "questionSubjects" : "wow",
-//             "boost": 3
-//         } },
-//         {
-//             "match" : {
-//                 "question" : "oi",
-//                 "boost": 2
-//         } }
+// "query": {
+//     "bool": {
+//       "should": [
+//         { "match": { "Question": {"query": "oi", "boost": 2 }}},
+//         { "match": { "QuestionSubjects": {"query":"intro", "boost": 3  }}}
 //       ]
 //     }
 //   }
+// }
